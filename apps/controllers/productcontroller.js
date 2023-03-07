5b9f6b794a679fd90385a8e05e4abe5cd8e4c7ee
Vo Huy Hoang <huyhoangvo2k1@gmail.com>
2023-03-07 09:53:25 +0700

    add page product

--- apps/controllers/productcontroller.js
+++ apps/controllers/productcontroller.js
@@ -1,17 +1,7 @@
 var express = require("express");
-var router = express();
-var Product = require("./../model/product")
+var router = express.Router();
+
 router.get("/", function (req, res) {
-    res.json({ "message": "this is product page" });
-});
-router.get("/get-product-list", function(req,res){
-    var productList = new Array();
-    for(var i = 0; i< 20;i++){
-        var product = new Product();
-        product.id = (i+1);
-        product.name = "name " + (i+1);
-        productList.push(product);
-    }
-    res.json(productList);
+    res.render("product.ejs");
 });
 module.exports = router;
